525bbe3f48755ed16366895964446f9b1e253358
Shane Connor <shane@tech1-167s.sutter.craigslist.org>
2016-04-18 14:11:25 -0700

    Adding threading

--- juniper_ae.py
+++ juniper_ae.py
@@ -4,6 +4,7 @@ import pexpect
 import datetime
 import getpass
 import re
+import threading
 
 __author__ = ['shane']
 __version__ = "April 2016"
@@ -32,6 +33,50 @@ switch_ae = {
 'switch2-ex':['ae1','ae3','ae5','ae10','ae11'],
 }
 
+# Ae bundles without any downed links 
+
+full_state = {                                                                  
+'switch1-ex_ae1':'10Gbps,',                                   
+'switch1-ex_ae3':'30Gbps,',                                   
+'switch1-ex_ae5':'20Gbps,',                                   
+'switch1-ex_ae10':'20Gbps,',                                  
+'switch1-ex_ae11':'20Gbps,',
+'switch2-ex_ae1':'10Gbps,',                                   
+'switch2-ex_ae3':'30Gbps,',                                   
+'switch2-ex_ae5':'20Gbps,',                                   
+'switch2-ex_ae10':'20Gbps,',                                  
+'switch2-ex_ae11':'20Gbps,',
+}
+
+def juniper_ae(switch,ae):                                                      
+    juniper_ae = pexpect.spawn('ssh -p 22 -o "StrictHostKeyChecking=no" %s'\    
+     %switch)                                                                   
+    juniper_ae.maxread = 5000                                                   
+    juniper_ae.expect('[pP]assword:')                                           
+    juniper_ae.sendline('%s' %password)                                         
+    juniper_ae.expect('>')                                                      
+    # loop through ae interfaces                                                
+    for a in ae:                                                                
+        juniper_ae.sendline ('show interfaces %s | match Speed' % a)            
+        juniper_ae.expect(' ')                                                  
+        juniper_ae.expect('{master.*}')                                         
+        juniper_ae_speed =  juniper_ae.before                                   
+        # match on edge switches                                                
+        if re.match('switch.*',switch):                                        
+            full_state_key = switch+'_'+a                                       
+            if (full_state[full_state_key] != juniper_ae_speed.split(' ')\      
+            [-4].rstrip()):                                                     
+                print "%s, %s speed" % (switch,a),juniper_ae_speed.split(' ')\  
+                [-4].rstrip()                                                   
+        # else match on mx switches                                             
+        else:                                                                   
+            if re.match('30.*',juniper_ae_speed.split(' ')[-1].rstrip()):       
+                pass #  continue if edge ae bunldes are 30 gig                  
+            else:                                                               
+                print "%s, %s ae bundle degraded" % (switch,a)                  
+    juniper_ae.expect('>')                                                      
+    juniper_ae.sendline('exit')
+
 if __name__ == '__main__':
 
     # grab password for use in pexpect
@@ -39,26 +84,7 @@ if __name__ == '__main__':
     # grap today's data and print it out
     today = datetime.date.today()
     print today
-
-    for switch, ae in sorted(switch_ae.items()):
-        juniper_ae = pexpect.spawn \
-        ('ssh -p 22 -o "StrictHostKeyChecking=no" %s' %switch)
-        juniper_ae.maxread = 5000
-        juniper_ae.expect('[pP]assword:')
-        juniper_ae.sendline('%s' %password)
-        juniper_ae.expect('>')
-        # loop through ae interfaces
-        for a in ae:
-            juniper_ae.sendline ('show interfaces %s | match Speed' % a)
-            juniper_ae.expect(' ')
-            juniper_ae.expect('{master.*}')
-            juniper_ae_speed =  juniper_ae.before
-            # different output from ex and mx's screen scraping
-            if re.match('switch.*',switch):
-                print "%s, %s speed" %(switch,a), juniper_ae_speed.split(' ')\
-                [-4].rstrip() 
-            else:
-                print "%s, %s speed" %(switch,a), juniper_ae_speed.split(' ')\
-                [-1].rstrip()
-        juniper_ae.expect('>')
-        juniper_ae.sendline('exit')
+    for switch, ae in sorted(switch_ae.items()):                                
+        t = threading.Thread(target=juniper_ae, args=(switch,ae,))              
+        threads.append(t)                                                       
+        t.start()  
